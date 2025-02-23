ebdce3fd89b11bdc8f2dbab1d9e34bb0da5f5c40
Levi <ilyeslev@outlook.hu>
2025-02-23 12:14:09 +0100

    test

--- dbt_tutorial/macros/generate_database_name.sql
+++ dbt_tutorial/macros/generate_database_name.sql
@@ -1,7 +1,6 @@
 {% macro generate_database_name(custom_database_name=none, node=none) -%}
 
-    {%- set default_database = default_database -%}
-    {%- if target.name == 'dev' and custom_database_name is not none -%}
+    {%- if target.name == 'prod' and custom_database_name is not none -%}
 
         PROD_{{ custom_database_name | trim }}
 
